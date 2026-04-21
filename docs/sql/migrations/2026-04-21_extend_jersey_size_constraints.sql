ae276dc37e69254d2c5cc3bdf9bf30fa193a5d73
ban9coga <yoga.fernandes42@gmail.com>
2026-04-21 18:14:18 +0700

    Extend jersey size DB constraints

---
+++ docs/sql/migrations/2026-04-21_extend_jersey_size_constraints.sql
@@ -0,0 +1,13 @@
+﻿alter table registration_items
+  drop constraint if exists ck_registration_items_jersey_size;
+
+alter table registration_items
+  add constraint ck_registration_items_jersey_size
+  check (jersey_size is null or jersey_size in ('XS','S','M','L','XL','2XL','3XL'));
+
+alter table riders
+  drop constraint if exists ck_rider_jersey_size;
+
+alter table riders
+  add constraint ck_rider_jersey_size
+  check (jersey_size is null or jersey_size in ('XS','S','M','L','XL','2XL','3XL'));
